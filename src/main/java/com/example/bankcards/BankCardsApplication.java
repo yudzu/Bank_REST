7e0fe86aec5059c55b272f528e2131860476c472
steph <van.stepa@mail.ru>
2025-06-19 14:48:27 +0300

    added dependencies, docker and database migration

---
+++ src/main/java/com/example/bankcards/BankCardsApplication.java
@@ -0,0 +1,11 @@
+package com.example.bankcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCardsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankCardsApplication.class, args);
+    }
+}
